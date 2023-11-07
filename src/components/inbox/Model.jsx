16f6a5af02a2efbeaabd00cbe3bda1b48b3943b6
Emran Hossain <emranhossain0335@gmail.com>
2023-11-07 09:14:25 +0600

    redux-app is getting its ui development

--- src/components/inbox/Model.jsx
+++ src/components/inbox/Model.jsx
@@ -1,12 +1,58 @@
-import React from 'react'
+import React from "react";
 
-const Model = () => {
+const Model = ({ open, control }) => {
   return (
     <>
-    <div className='w-full h-full inset-0 z-10 bg-black/50 cursor-pointer'>f</div>
-    <div className='absolute'>j</div>
+      {open && (
+        <>
+          <div className="w-full h-full inset-0 z-10 bg-black/50 cursor-pointer">
+            f
+          </div>
+          <div className="rounded w-[400px] lg:w-[600px] space-y-8 bg-white p-10 absolute top-1/2 left-1/2 z-20 -translate-x-1/2 -translate-y-1/2">
+            <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900"></h2>
+            <form className="mt-8 space-y-6">
+              <div className="rounded-md shadow-sm -space-y-px">
+                <div>
+                  <label htmlFor="to" className="sr-only">
+                    To
+                  </label>
+                  <input
+                    type="text"
+                    id="to"
+                    name="to"
+                    required
+                    placeholder="Send"
+                    className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-violet-500 focus:border-violet-500 focus:z-10 sm:text-sm"
+                  />
+                </div>
+                <div>
+                  <label htmlFor="message" className="sr-only">
+                    Message
+                  </label>
+                  <textarea
+                    name="messge"
+                    id="message"
+                    type="message"
+                    required
+                    placeholder="message"
+                    className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-violet-500 focus:border-violet-500 focus:z-10 sm:text-sm"
+                  ></textarea>
+                </div>
+              </div>
+              <div>
+                <button
+                  type="submit"
+                  className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md bg-violet-600 text-white hover:bg-violet-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-violet-500"
+                >
+                  Send Message
+                </button>
+              </div>
+            </form>
+          </div>
+        </>
+      )}
     </>
-  )
-}
+  );
+};
 
-export default Model
+export default Model;
--- src/components/inbox/Sidebar.jsx
+++ src/components/inbox/Sidebar.jsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import Model from "./Model";
 import ChatItems from "./ChatItems";
 import {FaEdit} from 'react-icons/fa'
 
 const Sidebar = () => {
+    const [open, setOpen] = useState(false)
   return (
     <div className="w-[100px] border border-t-0 border-gray-300 lg:col-span-1 md:w-full">
       <div className="h-[65px] text-center text-gray-500 p-4 border-b border-gray-300 flex md:justify-end justify-center">
@@ -12,7 +13,7 @@ const Sidebar = () => {
       <div className="overflow-auto h-[calc(100vh_-_129px)]">
         <ChatItems />
       </div>
-      <Model />
+      <Model open={open} control={setOpen}/>
     </div>
   );
 };
